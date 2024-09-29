a2f3ca22ee13c68b268967b66f2bd1755d9c6047
Aman Kumar <107541415+amankumar1804@users.noreply.github.com>
2024-09-30 01:51:09 +0530

    Update Projects.tsx

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -3,12 +3,17 @@ import projects from "@/data/content/projects";
 import ProjectCard from "./ProjectCard";
 
 function Projects({ overwriteProjects }) {
-  const projectsList = overwriteProjects ? overwriteProjects : projects;
+  const projectsList = overwriteProjects || projects;
+
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 items-start">
-      {projectsList.map(item => {
-        return <ProjectCard key={item.id} project={item} />;
-      })}
+      {projectsList.length === 0 ? (
+        <div>No projects available.</div>
+      ) : (
+        projectsList.map(item => (
+          <ProjectCard key={item.id} project={item} />
+        ))
+      )}
     </div>
   );
 }
